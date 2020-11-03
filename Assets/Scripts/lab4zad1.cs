ec6cc01cc5ad0889e181427eff8d46f5ee90b0ef
Dagmara <dagmarakabala1>
2020-11-03 22:52:28 +0100

    lab4

---
+++ Assets/Scripts/lab4zad1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class lab4zad1 : MonoBehaviour
+{
+    List<Vector3> positions = new List<Vector3>();
+    public float delay = 3.0f;
+    int objectCounter = 0;
+    // obiekt do generowania
+    public GameObject block;
+    public int howMuch = 10;
+    public GameObject plane;
+    public List<Material> materials;
+
+    void Start()
+    {
+        // w momecie uruchomienia generuje 10 kostek w losowych miejscach
+        List<int> pozycje_x = new List<int>(Enumerable.Range(0, 20).OrderBy(x => Guid.NewGuid()).Take(howMuch));
+        List<int> pozycje_z = new List<int>(Enumerable.Range(0, 20).OrderBy(x => Guid.NewGuid()).Take(howMuch));
+
+        for (int i = 0; i < howMuch; i++)
+        {
+            this.positions.Add(new Vector3(pozycje_x[i], 5, pozycje_z[i]));
+            //this.GetComponent<MeshRenderer>().material = materials[i];
+
+
+        }
+        foreach (Vector3 elem in positions)
+        {
+            Debug.Log(elem);
+        }
+        // uruchamiamy coroutine
+        StartCoroutine(GenerujObiekt());
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator GenerujObiekt()
+    {
+        Debug.Log("wywołano coroutine");
+        foreach (Vector3 pos in positions)
+        {
+            Instantiate(this.block, this.positions.ElementAt(this.objectCounter++), Quaternion.identity);
+
+            yield return new WaitForSeconds(this.delay);
+
+        }
+        // zatrzymujemy coroutine
+        StopCoroutine(GenerujObiekt());
+        
+
+    }
+
+}
---
+++ Assets/Scripts/lab4zad3.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using UnityEngine;
+
+public class lab4zad3 : MonoBehaviour
+{
+    // ruch wokół osi Y będzie wykonywany na obiekcie gracza, więc
+    // potrzebna nam referencja do niego (konkretnie jego komponentu Transform)
+    public Transform player;
+    float mouseXMove;
+    float mouseYMove;
+    float obrot;
+
+    public float sensitivity = 200f;
+
+    void Start()
+    {
+        // zablokowanie kursora na środku ekranu, oraz ukrycie kursora
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // pobieramy wartości dla obu osi ruchu myszy
+        mouseXMove = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        mouseYMove = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        obrot += mouseYMove;
+        player.Rotate(Vector3.up * mouseXMove);
+
+        if(obrot < 45 && obrot > -45)
+        {
+            transform.Rotate(new Vector3(-mouseYMove, 0f, 0f), Space.Self);
+        }
+
+    }
+}
